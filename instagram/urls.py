a83ab040bab609f0864b6f03e65a2cc5e854cc2f
Apondi-Otieno <apondiotii@gmail.com>
2022-04-05 10:21:03 +0300

    this will add navbar

---
+++ instagram/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+# URL Configurations
+urlpatterns = [
+    path("", views.login_request, name="login"),
+    path("homepage", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+    path("profile", views.profile,name = "profile"),
+    path("post", views.post, name="post"),
+    path("edit", views.edit,name = "edit"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
